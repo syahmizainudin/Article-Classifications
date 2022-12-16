eb3b9282d9f52aad8e5bab326f64d0cf8a552e8b
Syahmi Zainudin <msyahmizainudn@gmail.com>
2022-12-16 15:36:13 +0800

    Minor fixes

--- article_classifications.py
+++ article_classifications.py
@@ -58,21 +58,10 @@ df.duplicated().sum()
 # Remove unnecessary information
 STOPWORDS = set(stopwords.words('english'))
 
-def get_wordnet_pos(word):
-    tag = nltk.pos_tag([word])[0][1][0].upper()
-    tag_dict = {
-        "J": wordnet.ADJ,
-        "N": wordnet.NOUN,
-        "V": wordnet.VERB,
-        "R": wordnet.ADV
-    }
-
-    return tag_dict.get(tag, wordnet.NOUN)
-
 def clean_text(text, stop_words=STOPWORDS):
     lemmatizer = WordNetLemmatizer()
     filter = '[^\w]'
-    text = [lemmatizer.lemmatize(w, get_wordnet_pos(w)) for w in word_tokenize(text.lower()) if w not in stop_words]
+    text = [lemmatizer.lemmatize(w) for w in word_tokenize(text.lower()) if w not in stop_words]
     text = ' '.join(text)
     return re.sub(filter, ' ', text)
 
